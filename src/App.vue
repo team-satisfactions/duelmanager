13952f16b54baa82e2d0b32523fa1058bfa4a0fe
amanoese <amanoese@gmail.com>
2020-04-05 16:11:05 +0900

    view images

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,12 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <div style="display:flex; justify-content: space-around;">
-      <div v-for="(life,player) in lifes" :key="player">
-        <h1 style="text-align:start">{{player}}</h1>
-        <div class="font-big">LP {{life}}</div>
+      <div v-for="(life,player) in lifes" :key="player" style="position:relative;">
+        <img :src="'/' + player + '.jpeg'" :alt="player" style="width:400px;">
+        <div class="life-box">
+          <h1 style="text-align:start">{{player}}</h1>
+          <div class="font-big">LP {{life}}</div>
+        </div>
       </div>
     </div>
   </div>
@@ -39,12 +42,22 @@ export default {
 .font-big {
   font-size: 96px;
 }
+body {
+  height: 100vh;
+  margin: 0px;
+}
+.life-box {
+  width: 100%;
+  padding : 10px;
+  position: absolute;
+  bottom: 0;
+  background: rgba(255,255,255,0.8);
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
