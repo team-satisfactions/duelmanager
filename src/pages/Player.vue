26b4de062e3882c0720a586a2da21df0faf6f64a
higumachan <harekumo792154@gmail.com>
2020-05-06 16:33:49 +0900

    netlifyのredirectを作る

--- src/pages/Player.vue
+++ src/pages/Player.vue
@@ -1,72 +1,75 @@
 <template>
   <div>
-    <video v-if="isFoundOtherPlayerRTCId" id="their-video" height="100%" autoplay muted playsinline></video>
+    <video
+      v-if="isFoundOtherPlayerRTCId"
+      id="their-video"
+      height="100%"
+      autoplay
+      muted
+      playsinline
+    ></video>
     <h1 v-else>対戦相手がまだいません</h1>
   </div>
 </template>
 
 <script>
-  import skywayManager from "@/mixins/skywayManager";
-  import {createNamespacedHelpers} from "vuex";
-  import duel from "@/mixins/duel";
-  const { mapActions: mapDuelActions, mapState: mapDuelState } = createNamespacedHelpers("duel");
+import skywayManager from "@/mixins/skywayManager";
+import { createNamespacedHelpers } from "vuex";
+import duel from "@/mixins/duel";
+const {
+  mapActions: mapDuelActions,
+  mapState: mapDuelState
+} = createNamespacedHelpers("duel");
 
-
-  export default {
-    name: "Player",
-    mixins: [duel, skywayManager],
-    methods: {
-      ...mapDuelActions([
-        "setRTCId",
-        "bindPlayerRTCIds",
-      ]),
-      onPeerOpen() {
-        console.log("onp")
-        console.log(this.peer.id);
-        this.bindPlayerRTCIds().then(() => {
-          return this.setRTCId({
-            player: this.player,
-            RTCId: this.peer.id,
-          });
-        })
-      },
-      onGetMediaConnection(mediaConnection) {
-        mediaConnection.on('stream', stream => {
-          const videoElm = document.getElementById('their-video');
-          videoElm.srcObject = stream;
-          videoElm.play();
+export default {
+  name: "Player",
+  mixins: [duel, skywayManager],
+  methods: {
+    ...mapDuelActions(["setRTCId", "bindPlayerRTCIds"]),
+    onPeerOpen() {
+      console.log("onp");
+      console.log(this.peer.id);
+      this.bindPlayerRTCIds().then(() => {
+        return this.setRTCId({
+          player: this.player,
+          RTCId: this.peer.id
         });
-      },
-      onPeerCall(mediaConnection) {
-        mediaConnection.answer(this.localStream);
-        this.onGetMediaConnection(mediaConnection);
-      },
+      });
     },
-    computed: {
-      player() {
-        return "player" + this.$route.params.num;
-      },
-      otherPlayer() {
-        return "player" + ((parseInt(this.$route.params.num) - 1 + 1) % 2 + 1);
-      },
-      isFoundOtherPlayerRTCId() {
-        return this.playerRTCIds[this.otherPlayer] !== null;
-      },
-      ...mapDuelState(['playerRTCIds']),
+    onGetMediaConnection(mediaConnection) {
+      mediaConnection.on("stream", stream => {
+        const videoElm = document.getElementById("their-video");
+        videoElm.srcObject = stream;
+        videoElm.play();
+      });
     },
-    mounted() {
+    onPeerCall(mediaConnection) {
+      mediaConnection.answer(this.localStream);
+      this.onGetMediaConnection(mediaConnection);
+    }
+  },
+  computed: {
+    player() {
+      return "player" + this.$route.params.num;
     },
-    watch: {
-      playerRTCIds(newValue) {
-        const theirId = newValue[this.otherPlayer];
-        if (this.player === "player1" && theirId !== null) {
-          this.makeCall(theirId);
-        }
+    otherPlayer() {
+      return "player" + (((parseInt(this.$route.params.num) - 1 + 1) % 2) + 1);
+    },
+    isFoundOtherPlayerRTCId() {
+      return this.playerRTCIds[this.otherPlayer] !== null;
+    },
+    ...mapDuelState(["playerRTCIds"])
+  },
+  mounted() {},
+  watch: {
+    playerRTCIds(newValue) {
+      const theirId = newValue[this.otherPlayer];
+      if (this.player === "player1" && theirId !== null) {
+        this.makeCall(theirId);
       }
-    },
+    }
   }
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
